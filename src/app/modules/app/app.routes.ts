b2822f0009104fef49ce35986d8efb2381807526
Pavel Lasarev <paullasarev@gmail.com>
2017-03-12 08:07:30 +0400

    add root redirection

--- src/app/modules/app/app.routes.ts
+++ src/app/modules/app/app.routes.ts
@@ -3,7 +3,7 @@ import { CoursesComponent } from '../courses/courses.component';
 import { NoContentComponent } from './no-content/no-content.component';
 
 export const ROUTES: Routes = [
-  {path: '', component: CoursesComponent},
+  {path: '', pathMatch: 'full', redirectTo: '/courses'},
   {path: 'courses', component: CoursesComponent},
   {path: '**', component: NoContentComponent},
 ];
